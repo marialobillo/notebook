76c6e70ae9deb2a06136ff3e8f63061f66be04f1
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-09-03 16:46:38 +0200

    notes controller and routes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,41 @@
+class NotesController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def note_params
+
+    end
+
+    def find_note
+
+    end
+
+end

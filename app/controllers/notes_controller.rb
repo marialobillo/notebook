3145ddbb4dfe20a1e93662c342f45a13e6f855b9
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-09-05 13:57:58 +0200

    Add note views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,7 +3,7 @@ class NotesController < ApplicationController
   before_action :find_note, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @notes = Note.all.order('created_at DESC')
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'notes#index'
 
   resources :notes
 end
